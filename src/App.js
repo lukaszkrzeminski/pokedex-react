9bf220fc3987500ad7e18fea40a6ae07786b3cbf
Lukasz Krzeminski <krzeminski.lukasz@interia.pl>
2022-03-19 22:17:06 +0100

    added basic header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
+import Header from "./common/Header";
 
 function App() {
   return (
-    <div>
-      Hello World!
-    </div>
+    <Header />
   );
 }
 
---
+++ src/common/Header/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => (
+    <h1>
+        "Who's that Pokemon?!"
+    </h1>
+)
+
+export default Header;
