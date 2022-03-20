87406bc61cc54fbfee620bfce33b9f2bef00908b
Lukasz Krzeminski <krzeminski.lukasz@interia.pl>
2022-03-20 20:47:27 +0100

    added item list

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { lightTheme } from "./theme";
 import Header from "./common/Header";
 import Main from "./common/Main";
 import Buttons from "./features/Buttons";
+import ItemList from "./features/ItemList";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Main>
         <Header />
         <Buttons />
+        <ItemList />
       </Main>
     </ThemeProvider>
   );
