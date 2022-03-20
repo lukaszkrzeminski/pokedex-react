dcef4a066f6eabb9f15e99519465c13177faf8bf
Lukasz Krzeminski <krzeminski.lukasz@interia.pl>
2022-03-20 20:56:24 +0100

    minor main styles fixes

--- src/common/Main/styled.js
+++ src/common/Main/styled.js
@@ -4,4 +4,6 @@ export const MainContainer = styled.main`
     max-width: 1200px;
     margin: 20px auto;
     background-color: ${({ theme }) => theme.primaryColor};
+    border: 3px solid ${({ theme }) => theme.borderColor};
+    border-radius: 10px;
 `;
