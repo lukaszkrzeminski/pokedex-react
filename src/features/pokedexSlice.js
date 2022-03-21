bb7c59c5bb28e95769da811d0ad218007e7fa5ea
Lukasz Krzeminski <krzeminski.lukasz@interia.pl>
2022-03-21 22:50:10 +0100

    created slice

--- src/features/pokedexSlice.js
+++ src/features/pokedexSlice.js
@@ -1,13 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const pokedexSlice = createSlice({
-    name:"pokedex",
+    name:"pokemonList",
     initialState: {
         pokemonList: [],
+        loading: true,
     },
     reducers: {
-
+        fetchInitialPokemonList: (state) => {
+            state.loading = true;
+        },
+        fetchInitialPokemonListSuccess: (state, { payload: initialPokemonList }) => {
+            state.pokemonList = initialPokemonList;
+            state.loading = false;
+        },
+        fetchInitialPokemonListError: (state) => {
+            state.loading = false;
+        },
     },
 })
 
+export const {
+    fetchInitialPokemonList,
+    fetchInitialPokemonListSuccess,
+    fetchInitialPokemonListError,
+} = pokedexSlice.actions;
+
+const selectPokedexState = state => state.pokemonList;
+
+export const selectPokemonList = state => selectPokedexState(state).pokemonList;
 export default pokedexSlice.reducer;
