ad3bb8e0ad663f20f388d4d2354a2953c0591961
Lukasz Krzeminski <krzeminski.lukasz@interia.pl>
2022-03-21 21:16:43 +0100

    created slice template

---
+++ src/features/pokedexSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const pokedexSlice = createSlice({
+    name:"pokedex",
+    initialState: {
+        pokemonList: [],
+    },
+    reducers: {
+
+    },
+})
+
+export default pokedexSlice.reducer;
