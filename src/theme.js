e978110c4ac8c87b70b8e2658b2621ca7d273414
Lukasz Krzeminski <krzeminski.lukasz@interia.pl>
2022-03-23 21:46:41 +0100

    added darkTheme colors

--- src/theme.js
+++ src/theme.js
@@ -2,8 +2,20 @@ export const lightTheme = {
     primaryColor: '#E72600',
     secondaryColor: '#BDBDBD',
     primaryText: '#121212',
-    secondaryText: '#FFFFFF',
+    secondaryText: '#FFF',
     primaryButton: '#3783EB',
-    borderColor: '#000000',
+    borderColor: '#000',
     boxShadow: '#4F4F4F',
-}
+    backgroundColor: '#FFF'
+};
+
+export const darkTheme = {
+    primaryColor: '#330800',
+    secondaryColor: '#242424',
+    primaryText: '#FFF',
+    secondaryText: '#FFF',
+    primaryButton: '#072A5A',
+    borderColor: '#D0D0D0',
+    boxShadow: '#D0D1CC',
+    backgroundColor: '#191919'
+};
