acb1b4c5961f0f4b707f99094005b8ae5743a4b5
Lukasz Krzeminski <krzeminski.lukasz@interia.pl>
2022-03-23 20:19:03 +0100

    fetch function updated to get okemon content

--- src/features/api.js
+++ src/features/api.js
@@ -1,11 +1,32 @@
 export const getInitialPokemon = async () => {
     const response = await fetch("test-data.json");
 
-    if(!response.ok) {
+    if (!response.ok) {
         new Error(response.statusTest);
     }
 
     const initialData = await response.json();
+    const nextPage = await initialData.next;
+    const pokemonUrlList = [];
+    let i = 0;
+    for (const pokemon of initialData.results){
+        pokemonUrlList[i] = pokemon.url;
+        i++;
+    }
 
-    return initialData;
+    const pokemonData = [];
+    let j = 0;
+    for (const url of pokemonUrlList){
+        let dataResponse = await fetch(url);
+
+        pokemonData[j] = await dataResponse.json();
+        
+        if (!response.ok) {
+            new Error(response.statusTest);
+        }
+        j++;
+    }
+
+    const data = [nextPage, pokemonUrlList, pokemonData];
+    return data;
 };
